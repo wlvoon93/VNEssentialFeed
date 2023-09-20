d45981f30a43e7b1e3dfa76dbbc3fa4dfcf1d65a
Voon Wei Liang <weiliang.voon@myboost.co>
2023-09-20 22:46:20 +0800

    Create EssentialFeed framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Voon Wei Liang on 20/09/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
